46271b467dc2a91b08b0a0bcfbf4282f5fd34901
zhipeng.qin <8170608+zhipengqin@user.noreply.gitee.com>
2022-06-10 12:09:54 +0800

    testvue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+      <Tds></Tds>
+   </div>
+</template>
+
+<script>
+import Tds from './Tds.jsx'
+export default {
+   components: { Tds }
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
---
+++ src/Tds.jsx
@@ -0,0 +1,13 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+    setup() {
+
+        return () => {
+
+
+            return <div>9999</div>
+        }
+    }
+})
